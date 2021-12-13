f27b27ef18507263d5f1ccaf85846c3b320bef38
HirunimaSerasinghe <88132673+HirunimaSerasinghe@users.noreply.github.com>
2021-12-13 17:32:41 +0530

    add data file

---
+++ src/Pages/Data.js
@@ -0,0 +1,26 @@
+export const homeObjOne = {
+    lightBg : false,
+    topLine : 'Welcome!',
+    LightText : true,
+    LightTextDesc : true,
+    description : 'Quick Quiz Knowledge Booster For A Perfect Level Up ',
+    buttonLable : 'Log In',
+    alt : 'Credit card',
+    imgStart : '',
+    headline : 'Qui',
+    headline2:'zz',
+    headline3:'ee Qui',
+    headline4:'z',
+}
+
+
+export const homeObjTwo = {
+   Topline2 : 'Quick Quiz Knowledge Booster For A Perfect Level Up ',
+}
+
+export const SignUpFormData = {
+    Title:"Let’s Get Started!",
+    Subtitle:"Sign in to your new account of Quiz app",
+    Subtitle2:"Already have an account?",
+    Subtitle3:" Login here",
+}
